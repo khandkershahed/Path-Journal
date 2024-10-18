e43d8073eee9366991dd9314b0c55210abe107c3
shahed141123 <khandkershahed23@gmail.com>
2024-10-18 13:02:41 +0600

    18-10-24

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Exception;
 use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,14 +22,31 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    // public function boot(): void
+    // {
+    //     Paginator::useBootstrap();
+
+    //     $setting = Setting::pluck('value', 'key')->toArray();
+
+    //     View::composer('*', function($view) use ($setting){
+    //         $view->with('settings', $setting);
+    //     });
+    // }
+
     public function boot(): void
     {
+        // Set default values
+        View::share('settings', null);
+
+        try {
+            // Check for table existence and set actual values
+            if (Schema::hasTable('settings')) {
+                View::share('settings', Setting::pluck('value', 'key')->toArray());
+            }
+        } catch (Exception $e) {
+            // Log the exception if needed
+        }
+
         Paginator::useBootstrap();
-
-        $setting = Setting::pluck('value', 'key')->toArray();
-
-        View::composer('*', function($view) use ($setting){
-            $view->with('settings', $setting);
-        });
     }
 }
