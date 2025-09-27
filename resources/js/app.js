558a9c5988ebd6f6632c6139a9bee4bb1ef745d3
khandkershahed <khandkershahed23@gmail.com>
2025-09-28 01:02:32 +0600

    27-09-25

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,34 @@
+// import './bootstrap';
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+
 import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel';
+
 window.Alpine = Alpine;
 
 Alpine.start();
+
+$(function() {
+    $('.card__post-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        // Add or adjust options as you like
+    });
+});
